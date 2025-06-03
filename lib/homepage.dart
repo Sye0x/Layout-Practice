326f1a9d1311ef3da34dfef783e672e2a1b7603b
Sye0x <s.r.mahamid@gmail.com>
2025-06-03 22:00:21 +0500

    Practice 1

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
   const Homepage({super.key});
 
   @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
   Widget build(BuildContext context) {
+    final currentWidth = MediaQuery.of(context).size.width;
     return Scaffold(
+      backgroundColor:
+          currentWidth < 600
+              ? Colors.green
+              : const Color.fromARGB(255, 85, 29, 95),
       appBar: AppBar(title: Text('Homepage')),
-      body: Center(child: Text('Hello, World!')),
+      body: Center(child: Text('Current Width: $currentWidth')),
     );
   }
 }
